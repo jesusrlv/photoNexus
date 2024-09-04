e8f17135c2864318807e1f5d4a4f8d55fdd183aa
jesusrlv <jesusrlvrojo@gmail.com>
2024-09-04 12:46:58 -0600

    back photo

--- js/files.js
+++ js/files.js
@@ -2,9 +2,8 @@ function _(el) {
     return document.getElementById(el);
   }
   
-  function uploadFile(doc) {
-    var file = _("file"+doc).files[0];
-    var documento = doc;
+  function uploadFile() {
+    var file = _("file").files[0];
     // alert(file.name+" | "+file.size+" | "+file.type);
     var formdata = new FormData();
     // variable del name file
@@ -20,30 +19,30 @@ function _(el) {
     // http://www.developphp.com/video/JavaScript/File-Upload-Progress-Bar-Meter-Tutorial-Ajax-PHP
     //use file_upload_parser.php from above url
 
-    //ARCHIVO CON EL PROCEDIMIENTO PARA MOVER EL DOCUMENTO AL SERVIDOR
+    //ARCHIVO CON EL PROCEDIMIENTO PARA MOVER ELUMENTO AL SERVIDOR
     ajax.send(formdata);
 
     function progressHandler(event) {
 
-        _("loaded_n_total"+doc).innerHTML = "Cargado " + event.loaded + " bytes de " + event.total;
+        _("loaded_n_total").innerHTML = "Cargado " + event.loaded + " bytes de " + event.total;
         var percent = (event.loaded / event.total) * 100;
-        _("progressBar"+doc).value = Math.round(percent);
-        _("status"+doc).innerHTML = Math.round(percent) + "% subido... espere un momento";
+        _("progressBar").value = Math.round(percent);
+        _("status").innerHTML = Math.round(percent) + "% subido... espere un momento";
       }
       
       function completeHandler(event) {
-        _("status"+doc).innerHTML = event.target.responseText;
-        _("progressBar"+doc).value = 0; //wil clear progress bar after successful upload
-          _("file"+doc).style.display='none';
-          _("progressBar"+doc).style.display='none';
+        _("status").innerHTML = event.target.responseText;
+        _("progressBar").value = 0; //wil clear progress bar after successful upload
+          _("file").style.display='none';
+          _("progressBar").style.display='none';
       }
       
       function errorHandler(event) {
-        _("status"+doc).innerHTML = "Fallo en la subida";
+        _("status").innerHTML = "Fallo en la subida";
       }
       
       function abortHandler(event) {
-        _("status"+doc).innerHTML = "Fallo en la subida";
+        _("status").innerHTML = "Fallo en la subida";
       }
     
   }
